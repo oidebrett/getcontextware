ac2fd33267f9710546930285e4a588e182c01b54
oidebrett <oidebrett@lccgmail.com>
2025-06-12 10:54:25 +0200

    flag for db host

--- import_postgres.sh
+++ import_postgres.sh
@@ -8,7 +8,7 @@ fi
 # Import CSV data into PostgreSQL
 set -e
 
-PG_CONTAINER_NAME=$POSTGRES_PASSWORD
+PG_CONTAINER_NAME=$POSTGRES_HOST
 PG_IP=$(docker network inspect pangolin 2>/dev/null | \
     awk "/\"Name\": \"$PG_CONTAINER_NAME\"/,/IPv4Address/" | \
     grep '"IPv4Address"' | \
