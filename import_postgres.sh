8f09b8da8520554069488e68302375068a0267c4
oidebrett <oidebrett@lccgmail.com>
2025-06-12 11:16:31 +0200

    conditional standalone pg

--- import_postgres.sh
+++ import_postgres.sh
@@ -8,7 +8,7 @@ fi
 # Import CSV data into PostgreSQL
 set -e
 
-PG_CONTAINER_NAME=$POSTGRES_HOST
+PG_CONTAINER_NAME=${POSTGRES_HOST:-pangolin-postgres}
 PG_IP=$(docker network inspect pangolin 2>/dev/null | \
     awk "/\"Name\": \"$PG_CONTAINER_NAME\"/,/IPv4Address/" | \
     grep '"IPv4Address"' | \
